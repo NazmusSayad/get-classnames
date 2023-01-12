9ca37492bd9b21f802aa39e9293b4dfacd36f5a1
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-01-12 04:29:29 +0000

    group

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-const getClassNames = (...classNames: any[]): string => {
+import createClassFunction from 'class-function'
+
+function getClassNames(...classNames: any[]): string {
   let output: string = ''
 
   classNames.flat(Infinity).forEach((cn) => {
@@ -10,4 +12,19 @@ const getClassNames = (...classNames: any[]): string => {
   return output.trim().replace(/ {2,}/gim, ' ')
 }
 
-export default getClassNames
+class GetClassNames {
+  default = getClassNames
+
+  group(prefix: string, ...args: any[]): string {
+    const className = getClassNames(...args)
+    return className === ''
+      ? className
+      : prefix + className.replace(/ /gim, ' ' + prefix)
+  }
+}
+
+interface InstanceType extends GetClassNames {
+  (...args: any[]): string
+}
+
+export default createClassFunction<InstanceType>(GetClassNames)
